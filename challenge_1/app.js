28a7fdcabf1b5e682099f12cf8be50f0116525a6
Michael Nutt <mnutt346@gmail.com>
2018-12-10 15:10:23 -0600

    (feat) Squares may not be toggled more than once per game.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,12 +2,30 @@
 
 let board = document.getElementById('board');
 
+let xTurn = true;
+
+// Update squares on click
+
 let squareClickHandler = function (elem) {
-    elem.innerHTML = 'X';
+    if (elem.clicked) {
+        return;
+    } else if (xTurn) {
+        let thisNic = '<img style="vertical-align: bottom;" width="100%" height="100%" src="thisNic.jpg" />'
+        elem.innerHTML = thisNic;
+        elem.removeEventListener('click', arguments.callee, true);
+        elem.clicked = true;
+        xTurn = !xTurn;
+    } else {
+        let thatNic = '<img style="vertical-align: bottom;" width="100%" height="100%" src="thatNic.jpg" />'
+        elem.innerHTML = thatNic;
+        elem.clicked = true;
+        xTurn = !xTurn;
+    }
 }
 
 for (let i = 0; i < board.rows.length; i++) {
     for (let j = 0; j < board.rows[i].cells.length; j++) {
+        board.rows[i].cells[j].clicked = false;
         board.rows[i].cells[j].onclick = function () {
             squareClickHandler(this);
         }
@@ -16,6 +34,6 @@ for (let i = 0; i < board.rows.length; i++) {
 
 
 
-// square.addEventListener('click', squareClickHandler.bind(this));
+
 
 console.log("HELLOOOOO")
