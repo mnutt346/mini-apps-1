73429c0e547d3fd1d7cfe6e35c97b6bce3e11653
Michael Nutt <mnutt346@gmail.com>
2018-12-10 12:37:08 -0600

    (feat) Table cells toggle to 'X' on click.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,21 @@
 
 
+let board = document.getElementById('board');
+
+let squareClickHandler = function (elem) {
+    elem.innerHTML = 'X';
+}
+
+for (let i = 0; i < board.rows.length; i++) {
+    for (let j = 0; j < board.rows[i].cells.length; j++) {
+        board.rows[i].cells[j].onclick = function () {
+            squareClickHandler(this);
+        }
+    }
+}
+
+
+
+// square.addEventListener('click', squareClickHandler.bind(this));
+
+console.log("HELLOOOOO")
