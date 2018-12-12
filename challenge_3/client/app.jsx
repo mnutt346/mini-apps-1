cfb7003ea07b07f75be9eefdf5424dab598f6e72
Michael Nutt <mnutt346@gmail.com>
2018-12-12 12:40:04 -0600

    Babel transpiles to app.js.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,10 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
 
+// class App extends React.Component {
+//   render () {
+//     return <h1>HOME PAGE</h1>
+//   }
+// }
 
+ReactDOM.render(<h1>HELLOOO</h1>, document.getElementById('app'))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const path = require('path')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+const port = 3000
+
+app.use(express.static('public'))
+
+app.listen(port, function () {
+  console.log(`Server listening on port ${port}`)
+})
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/public/index.html'))
+// })
